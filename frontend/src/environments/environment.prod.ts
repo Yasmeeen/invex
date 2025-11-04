587bf93100fc123e13aff7c6055093b0acbe6ce1
Yasmeeen <yasminabdelhay7@gmail.com>
2025-11-04 19:51:50 +0200

    adjust enviroment

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
     env: 'production',
     storeName:"OX Muscle store",
     storePhoneNumber: "01090990378",
-    apiUrl: 'http://ec2-16-171-67-19.eu-north-1.compute.amazonaws.com/' 
+    apiUrl: 'http://ec2-16-171-67-19.eu-north-1.compute.amazonaws.com:3000' 
 
 }

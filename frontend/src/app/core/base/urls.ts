e7cd4b6aa9095dbad900a915e22f2a48906d425f
Yasmeeen <yasminabdelhay7@gmail.com>
2025-12-27 20:07:55 +0200

    remove api

--- frontend/src/app/core/base/urls.ts
+++ frontend/src/app/core/base/urls.ts
@@ -5,45 +5,45 @@ export const BASE_URL = environment.apiUrl
 
 
 
-export const USER_LOGIN_URL = BASE_URL + '/api/users/login';
-export const USER_REGISTER_URL = BASE_URL + '/api/users/register';
-export const USER_UPDATE_PASSWORD_URL = BASE_URL + '/api/users/updatePassword';
+export const USER_LOGIN_URL = BASE_URL + '/users/login';
+export const USER_REGISTER_URL = BASE_URL + '/users/register';
+export const USER_UPDATE_PASSWORD_URL = BASE_URL + '/users/updatePassword';
 
-export const USERS_URL = BASE_URL + '/api/users';
+export const USERS_URL = BASE_URL + '/users';
 export const USER_CREATE_URL = USERS_URL + '/createUser';
 export const USER_UPDATE_URL = USERS_URL;
 export const USER_DELETE_URL = USERS_URL;
 
 //dashboard
-export const ORDER_STATISTICS = BASE_URL + '/api/dashboard/getOrdersStatstics';
+export const ORDER_STATISTICS = BASE_URL + '/dashboard/getOrdersStatstics';
 
 
 
-export const PRODUCTS_URL = BASE_URL + '/api/products';
+export const PRODUCTS_URL = BASE_URL + '/products';
 export const PRODUCT_CREATE_PRODUCT_URL = PRODUCTS_URL+ '/createProduct';
 export const PRODUCT_UPDATE_PRODUCT_URL = PRODUCTS_URL ;
 export const PRODUCT_DELETE_PRODUCT_URL = PRODUCTS_URL + '/deleteProduct';
 export const PRODUCT_STATS =  PRODUCTS_URL +'/getProductsStats'
 
-export const CATEGORYS_URL = BASE_URL + '/api/categories';
+export const CATEGORYS_URL = BASE_URL + '/categories';
 export const CATEGORY_CREATE_CATEGORY_URL = CATEGORYS_URL+ '/createCategory';
 export const CATEGORY_UPDATE_CATEGORY_URL = CATEGORYS_URL + '/updateCategory';
 export const CATEGORY_DELETE_CATEGORY_URL = CATEGORYS_URL + '/deleteCategory';
 
-export const BRANCHES_URL = BASE_URL + '/api/branches';
+export const BRANCHES_URL = BASE_URL + '/branches';
 export const BRANCH_CREATE_BRANCH_URL = BRANCHES_URL+ '/createBranch';
 export const BRANCH_UPDATE_BRANCH_URL = BRANCHES_URL + '/updateBranch';
 export const BRANCH_DELETE_BRANCH_URL = BRANCHES_URL + '/deleteBranch';
 
 
-export const ORDERS_URL = BASE_URL + '/api/orders';
+export const ORDERS_URL = BASE_URL + '/orders';
 export const ORDER_CREATE_URL = ORDERS_URL + '/createOrder';
 export const ORDER_UPDATE_URL = ORDERS_URL + '/updateOrder';
 
 
-export const VENDORS_URL = BASE_URL + '/api/vendors';
+export const VENDORS_URL = BASE_URL + '/vendors';
 
-export const PURCHASING_URL = BASE_URL + '/api/purchasing';
+export const PURCHASING_URL = BASE_URL + '/purchasing';
 
 export const ORDER_PAY_URL = ORDERS_URL + '/pay';
 export const ORDER_TRACK_URL = ORDERS_URL + '/track/';
